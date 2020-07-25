ca071a6a7c735c65006c8b80324abde9f1fbc04c
Filipe Brandenburger <filbranden@gmail.com>
2020-07-25 10:34:23 -0700

    Update Subscription to refer to the User model.
    Update both the model and the migration (which hadn't been executed yet.)
    This is required on Rails 5+ so that the indices are created correctly.
    
    $ bin/rails db:migrate

--- db/migrate/20200725173115_create_subscriptions.rb
+++ db/migrate/20200725173115_create_subscriptions.rb
@@ -1,8 +1,8 @@
 class CreateSubscriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :subscriptions do |t|
-      t.references :leader, null: false, foreign_key: true
-      t.references :follower, null: false, foreign_key: true
+      t.references :leader, null: false, foreign_key: { to_table: :users }
+      t.references :follower, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
