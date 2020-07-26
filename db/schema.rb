51481478397e10e265076350a6f652e1702df434
Filipe Brandenburger <filbranden@gmail.com>
2020-07-25 21:04:36 -0700

    Add password digest to Users.
    $ bin/rails generate migration AddPasswordDigestToUsers password_digest
    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_174808) do
+ActiveRecord::Schema.define(version: 2020_07_26_040401) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_07_25_174808) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
