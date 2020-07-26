6fed872952dccd01f52de6fd16ff5c9a07dcb7f8
Filipe Brandenburger <filbranden@gmail.com>
2020-07-26 08:53:10 -0700

    Add a root route to index of Posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+# For details on the DSL available within this file, see
+# https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   resources :comments
   resources :image_posts
   resources :text_posts
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
 end
