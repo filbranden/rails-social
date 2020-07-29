165df1619494241e4ff634a739b6cbf6ca773e52
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 12:47:02 -0700

    Add integration test for the user flow.
    Log in, browse a page, then log out.
    
    $ bin/rails test:integration

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserFlowTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user login, browse and logout" do
+    user = users(:user1)
+
+    get '/login'
+    assert_response :success
+
+    post '/sessions', params: {
+      email: user.email,
+      password: 'password'
+    }
+    follow_redirect!
+    assert_response :success
+    assert_equal '/', path
+
+    get '/logout'
+    follow_redirect!
+    assert_equal '/', path
+    follow_redirect!
+    assert_response :success
+    assert_equal '/login', path
+  end
 end
