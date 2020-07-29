a64dd163f28b2695752793e56071d0033d30ca25
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 12:14:05 -0700

    Test the User controller, check unauthenticated user sent to login page.
    Tested with:
    $ bin/rails test:controllers

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "redirects anonymous users to login" do
+    get root_url
+    assert_redirected_to login_url
+  end
 end
