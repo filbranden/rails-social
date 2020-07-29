41b89c03045bc4f5d8dc1d2cca532b9324389d78
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 12:32:55 -0700

    Add an integration test for authenticated User.
    $ bin/rails test:controllers

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,4 +5,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_redirected_to login_url
   end
+
+  def login_as(user, password)
+    post sessions_url, params: { email: user.email, password: password }
+    assert_redirected_to root_url
+  end
+
+  test "get posts for authenticated users" do
+    user1 = users(:user1)
+
+    login_as user1, 'password'
+
+    get root_url
+    assert_response :success
+  end
 end
