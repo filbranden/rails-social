20fb39de29bdafef700cc1e0754770b6f2e997c7
Filipe Brandenburger <filbranden@gmail.com>
2020-07-28 19:29:38 -0700

    Refactor tests to initialize users at `setup`.
    Confirmed that test cases still pass:
    $ bin/rails test test/models/user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+  end
+
   test "saves with valid attributes" do
     user = User.new(
       email: "user@example.com",
@@ -11,25 +16,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "validates email presence" do
-    @user1 = users(:user1)
     @user1.email = nil
 
     assert_not @user1.valid?
   end
 
   test "validates email uniqueness" do
-    @user1 = users(:user1)
-    @user2 = users(:user2)
-
     @user1.email = @user2.email
 
     assert_not @user1.valid?
   end
 
   test "should follow leader" do
-    @user1 = users(:user1)
-    @user2 = users(:user2)
-
     @user1.follow!(@user2)
 
     assert_equal 1, @user1.leaders.count
@@ -37,9 +35,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "following? should be true" do
-    @user1 = users(:user1)
-    @user2 = users(:user2)
-
     @user1.follow!(@user2)
 
     assert @user1.following?(@user2)
