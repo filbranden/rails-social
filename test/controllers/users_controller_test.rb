b8c0334cdd73fb01360cc7a57f960f0328511f78
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 18:00:39 -0700

    Have Follow action require an authenticated user.
    Add a test to ensure accessing that action without authentication will
    redirect to the login form.
    
    Also started setting Ruby global options to disable deprecation warnings:
    $ export RUBYOPT=-W0
    
    Running all the tests:
    $ bin/rails test
    
    (Chapter 10, Exercise 2.)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,4 +40,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert user1.following? user2
     assert_redirected_to user_url(user2)
   end
+
+  test "follow should require login" do
+    user2 = users(:user2)
+
+    get follow_user_url(user2)
+    assert_redirected_to login_url
+  end
 end
