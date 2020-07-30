5641d56bc9c6022259ce0d63b9e8710d4c0e8c9c
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 18:16:32 -0700

    Add plumbing to create new image post.
    (Chapter 10, Exercise 3.)

--- test/controllers/image_posts_controller_test.rb
+++ test/controllers/image_posts_controller_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ImagePostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def login_as(user, password)
+    post sessions_url, params: { email: user.email, password: password }
+    assert_redirected_to root_url
+  end
+
+  test "get new with new post" do
+    user1 = users(:user1)
+    login_as user1, 'password'
+
+    get new_image_post_url
+    assert_response :success
+  end
+
+  test "should create post" do
+    user1 = users(:user1)
+    login_as user1, 'password'
+
+    post image_posts_url, params: {
+      image_post: {
+        title: "Test Image Title",
+        url: "http://img.example.com/test.jpg"
+      }
+    }
+
+    post = Post.last
+    assert_equal "Test Image Title", post.title
+    assert_equal "http://img.example.com/test.jpg", post.url
+    assert_nil post.body
+    assert_equal user1.id, post.user_id
+    assert_redirected_to post_url(post)
+  end
 end
