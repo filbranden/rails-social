64c3487079765f52af91baa1669b0c9ce353cd0a
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 17:26:33 -0700

    Add a form to create a new text post, using TDD
    Test suite passes after adding controller action and views:
    $ bin/rails test:controllers
    
    Submitting the form still doesn't work, the create action is still
    missing, coming up next.

--- test/controllers/text_posts_controller_test.rb
+++ test/controllers/text_posts_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TextPostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def login_as(user, password)
+    post sessions_url, params: { email: user.email, password: password }
+    assert_redirected_to root_url
+  end
+
+  test "get new with new post" do
+    user1 = users(:user1)
+    login_as user1, 'password'
+
+    get new_text_post_url
+    assert_response :success
+  end
 end
