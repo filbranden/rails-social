163429832c057e4cb145b2bb7cbbd4ddee9617c7
Filipe Brandenburger <filbranden@gmail.com>
2020-07-29 17:35:41 -0700

    Implement the create post action using TDD.
    Test passes:
    $ bin/rails test:controllers
    
    Also tested manually, creating a post through the form.

--- test/controllers/text_posts_controller_test.rb
+++ test/controllers/text_posts_controller_test.rb
@@ -13,4 +13,22 @@ class TextPostsControllerTest < ActionDispatch::IntegrationTest
     get new_text_post_url
     assert_response :success
   end
+
+  test "should create post" do
+    user1 = users(:user1)
+    login_as user1, 'password'
+
+    post text_posts_url, params: {
+      text_post: {
+        title: "Test Title",
+        body: "Test Body"
+      }
+    }
+
+    post = Post.last
+    assert_equal "Test Title", post.title
+    assert_equal "Test Body", post.body
+    assert_equal user1.id, post.user_id
+    assert_redirected_to post_url(post)
+  end
 end
