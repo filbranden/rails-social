8b7abf5d8a41253a43fa274b7159682d459aa3e8
Filipe Brandenburger <filbranden@gmail.com>
2020-07-30 18:21:23 -0700

    Add comment indicating why protect_from_forgery is not needed.
    CSRF protection is enabled by default in Rails 5.2+.
    
    See also:
    https://stackoverflow.com/q/55860171/9447571

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  # An explicit call to protect_from_forgery to implement CSRF is no longer
+  # needed in Rails 5.2+, since it is now enabled by default through
+  # configuration.
+  #
+  # To inspect that configuration:
+  #
+  #   $ bin/rails console
+  #   irb(main):001:0> RailsSocial::Application::config.action_controller.default_protect_from_forgery
+  #   => true
+
   private
 
   def current_user
