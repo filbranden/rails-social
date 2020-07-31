532ad8c8881922046b4b15c7d6cf24a3174f5e3d
Filipe Brandenburger <filbranden@gmail.com>
2020-07-31 15:32:24 -0700

    Add an API token field to Users.
    In order to authenticate to the JSON API.
    
    $ bin/rails generate migration add_api_token_to_users api_token:string
    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_210404) do
+ActiveRecord::Schema.define(version: 2020_07_31_223208) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_210404) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "api_token"
   end
 
   add_foreign_key "comments", "posts"
