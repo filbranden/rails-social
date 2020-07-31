27c2fe637259c405a43a54ac72f538a9e4130194
Filipe Brandenburger <filbranden@gmail.com>
2020-07-31 13:57:45 -0700

    Add a new comments_cache column to Posts table.
    We will cache comment counts there, rather than on disk.
    
    $ bin/rails generate migration AddCommentsCountToPosts comments_count:integer
    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_040401) do
+ActiveRecord::Schema.define(version: 2020_07_31_205720) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_07_26_040401) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "comments_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
