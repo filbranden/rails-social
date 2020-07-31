681d0643db5a88171935451b9668f6c8236f4c10
Filipe Brandenburger <filbranden@gmail.com>
2020-07-31 13:25:11 -0700

    Cache number of comments in the Post model.
    Update the Comment model relationship to "touch" a Post whenever a new
    Comment is added.
    
    Confirmed this works when adding a new comment.
    
    Note: We should also explore using `counter_cache: true` to have Rails
    take care of this automatically.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, touch: true
   belongs_to :user
   validates :post_id, presence: true
   validates :user_id, presence: true
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :user_id, presence: true
   validates :type, presence: true
+
+  def cached_comment_count
+    Rails.cache.fetch [self, "comment_count"] do
+      comments.size
+    end
+  end
 end
