e1ec41d6ba5604e6f62346a2a53b8f04df8e8789
Filipe Brandenburger <filbranden@gmail.com>
2020-07-31 15:14:48 -0700

    Include full User in Post JSON data.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :user_id, presence: true
   validates :type, presence: true
+
+  def as_json(options={})
+    super(except: [:user_id], include: :user)
+  end
 end
