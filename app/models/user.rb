9774240252f3d504d109c128d2a0557aa319f1db
Filipe Brandenburger <filbranden@gmail.com>
2020-07-31 15:37:09 -0700

    Generate API Token at user creation.
    Also display the API Token when the user is looking at their own page.
    
    Generate API Tokens for the existing users, with:
    
    $ bin/rails console
    irb(main):001:0> User.find_each {|user| user.save}

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,9 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  validates :api_token, presence: true, uniqueness: true
+
+  before_validation :generate_api_token
 
   def following?(leader)
     leaders.include? leader
@@ -27,5 +30,13 @@ class User < ApplicationRecord
 
   def timeline_user_ids
     leader_ids + [id]
+  end
+
+  def generate_api_token
+    return if api_token.present?
+    loop do
+      self.api_token = SecureRandom.hex
+      break unless User.exists? api_token: api_token
+    end
   end
 end
