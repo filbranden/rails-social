e12e690df4d8ce0015ee318f25fc0cda6b7a9798
Filipe Brandenburger <filbranden@gmail.com>
2020-07-31 13:11:50 -0700

    Add pagination to Posts.
    Display 5 posts per page.
    
    Use the `will_paginate` Ruby Gem to implement it.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   def index
     user_ids = current_user.timeline_user_ids
     @posts = Post.includes(:user).where(user_id: user_ids)
+               .paginate(page: params[:page], per_page: 5)
                .order("created_at DESC")
   end
 
